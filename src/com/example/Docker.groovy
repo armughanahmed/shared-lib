4c4e878e6766ec506f4453902bc67aec4d1d3318
armughanahmed <armughan.ahmed@yahoo.com>
2021-03-29 16:26:33 +0500

    shared

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,39 @@
+#!/usr/bin/env groovy
+package com.example
+
+class Docker implements Serializable {
+
+    def script
+
+    Docker(script) {
+        this.script = script
+    }
+
+    def buildDockerImage(String imageName) {
+        // script.echo 'building the docker image...'
+        // script.sh "docker build -t $imageName ."
+        // script.withCredentials([script.usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        //     script.docker.withRegistry('https://registry.hub.docker.com', 'dockerhub') {
+        //         script.docker.build("$script.USER/$imageName", '.').push()
+        //     }
+        script.echo 'building the docker image...'
+        script.sh "docker build -t $imageName ."
+        }
+    def dockerLogin() {
+        script.withCredentials([script.usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+        }
+    }
+
+    // def dockerLogin() {
+    //     script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+    //         script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+    //     }
+    // }
+
+    def dockerPush(String imageName) {
+        script.sh "docker push $imageName"
+    }
+
+    }
+
